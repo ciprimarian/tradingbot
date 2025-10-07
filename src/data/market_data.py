b8934f272465613f6eedf88891897cdc49d353cd
McMarian <ciprianissam@gmail.com>
2025-10-07 23:09:03 +0200

    market data fetching

---
+++ src/data/market_data.py
@@ -0,0 +1,50 @@
+# src/data/market_data.py
+
+import requests
+import pandas as pd
+from config import settings
+
+class MarketData:
+    def __init__(self):
+        self.base_url = settings.BASE_URL
+        self.headers = {
+            'APCA-API-KEY-ID': settings.ALPACA_API_KEY,
+            'APCA-API-SECRET-ID': settings.ALPACA_SECRET_KEY
+        }
+        print("market Data handler initialized")
+
+    def get_historical_bars(self, symbol: str, timeframe: str, start: str, limit: int = 100):
+        endpoint = f"/v2/stocks/{symbol}/bars"
+        parms = {
+            "tymeframe": timeframe,
+            "start": start,
+            "limit": limit,
+            "adjustment": "raw" #for pure price data
+        }
+        try:
+            print(f"Fetching {limit} bars for {symbol} with timeframe {timeframe} starting from {start}...")
+            response = requests.get(f"{self.base_url}{endpoint}")
+            response.raise_for_status()
+
+            data = response.json()
+
+            df = pd.DataFrame(data['bars'])
+            if df.empty:
+                print(f"No data returned for {symbol}. The symbol might be incored or no data available for the period")
+                return None
+            
+            df['t'] = pd.to_datetime(df['t'])
+            df.set_index('t', inplace=True)
+            df.rename(columns={'o': 'open', 'h': 'high', 'l': 'low', 'c': 'close', 'v': 'volume'}, inplace=True)
+            df = df[['open', 'high', 'low', 'close,', 'volume']]
+            
+            print("Succesfully fetched and processed data")
+            return df
+        except requests.exceptions.HTTPError as err:
+            print(f"HTTP Error: {err}")
+            print("Response Body:", err.response.text)
+            return None
+        except Exception as e:
+            print(f"AN unexpected error occured: {e}")
+            return None
+
