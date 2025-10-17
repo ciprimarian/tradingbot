d63df240a44d091536d661ffe2ad691a0553cc1a
McMarian <ciprianissam@gmail.com>
2025-10-17 18:20:32 +0200

    Small Corections

--- src/config/settings.py
+++ src/config/settings.py
@@ -12,7 +12,7 @@ ALPACA_API_KEY = os.getenv('ALPACA_API_KEY')
 ALPACA_SECRET_KEY = os.getenv('ALPACA_SECRET_KEY')
 
 # Determine if the mode is paper trading and set the correct URL
-IS_PAPER_TRADING = os.getenv('IS_PAPER_TRADING', 'True').lower() in ('true', '1', 't')
+IS_PAPER_TRADING = os.getenv('ALPACA_PAPER_TRADING', 'True').lower() in ('true', '1', 't')
 
 if IS_PAPER_TRADING:
     BASE_URL = 'https://paper-api.alpaca.markets'
@@ -20,5 +20,5 @@ else:
     BASE_URL = 'https://api.alpaca.markets'
 
     # Check if the API keys are loaded correctly
-    if not ALPACA_API_KEY or not ALPACA_SECRET_KEY:
-        raise ValueError("ALPACA_API_KEY and ALPACA_SECRET_KEY must be set in the .env file.")
+if not ALPACA_API_KEY or not ALPACA_SECRET_KEY:
+    raise ValueError("ALPACA_API_KEY and ALPACA_SECRET_KEY must be set in the .env file.")
