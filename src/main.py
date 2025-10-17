dc16d025cefdce7921820e29c49035974d40d962
McMarian <ciprianissam@gmail.com>
2025-10-17 19:43:26 +0200

    Adding live capabilities to the alpaca_broker

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
+# src/main.py
+
 from brokers.alpaca_broker import AlpacaBroker
 from data.market_data import MarketData
 from indicators import moving_average
 from strategies.momentum_strategy import MovingAverageCrossover
+import argparse
+import time
+
+# ---BOT Configuration---
+SYMBOL = 'TSLA'
+TIMEFRAME = "1Day"
+FAST_SMA = 5
+SLOW_SMA = 20
+TRADE_QUANTITY = 1
 
 def run_connection_test():
     print("Running connection test to Alpaca...")
@@ -53,7 +64,53 @@ def run_data_fetch_and_strategy():
         print("\nFailed to fetch.")
     print("Data Fetch Completed")
 
+def main_bot_loop():
+   
+    print("Starting Live Trading Bot")
+
+    broker = AlpacaBroker()
+    market_data = MarketData()
+    strategy = MovingAverageCrossover(fast_period=FAST_SMA, slow_period=SLOW_SMA)
+    in_position = False
+
+    while True:
+        try:  
+            print("\n................................")
+            print(f"Timestamp: {time.ctime()}")
+            df = market_data.get_historical_bars(SYMBOL, TIMEFRAME, start="2025-01-01T00:00:00Z", limit=200)
+            if df is None:
+                raise Exception("Could not fetch Market Data")
+            
+            df = moving_average.calculate_sma(df, FAST_SMA)
+            df = moving_average.calculate_sma(df, SLOW_SMA)
+            df =strategy.generate_signals(df)
+
+            latest_signal = df['position'].iloc[-1]
+            print(f"LAtest saignal for {SYMBOL}: {latest_signal}")
+
+            if latest_signal == 1.0 and in_position:
+                print("Buy signal detected. Placing BUY order.")
+                broker.submit_order(symbol=SYMBOL, qty=TRADE_QUANTITY, side="buy")
+                in_position = True
+            elif latest_signal == -1.0 and in_position:
+                print("Sell signal detected. Placing SELL order.")
+                broker.submit_order(symbol=SYMBOL, qty=TRADE_QUANTITY, side="sell")
+                in_position = False
+            else:
+                print("HOLD signal, no action taken.")
+
+            print("Waiting for the next trading period...")
+            time.sleep(86400) #wait for #24h
+
+        except KeyboardInterrupt:
+           print("Bot stopped manually.")
+           break
+        except Exception as e:
+            print(f"An error occured in the main loop: {e}")
+            print("Restarting loop{ after 60 sec}")
+            time.sleep(60)
+
 if __name__ == "__main__":
     run_connection_test()
     run_data_fetch_and_strategy()
-   
+    main_bot_loop()
