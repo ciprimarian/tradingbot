4a3e9f3f88a354ffbc05263fc434089ad4eec4f6
McMarian <ciprianissam@gmail.com>
2025-10-17 14:48:44 +0200

    moving average calculation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from brokers.alpaca_broker import AlpacaBroker
 from data.market_data import MarketData
+from indicators import moving_average
 
 def run_connection_test():
     print("Running connection test to Alpaca...")
@@ -13,12 +14,21 @@ def run_data_fetch():
     historical_data = market_data_handler.get_historical_bars(
         symbol="TSLA",
         timeframe="1Day",
-        start="2025-01-01T00:00:00Z"
+        start="2025-01-01T00:00:00Z",
+        limit=50
     )
 
     if historical_data is not None:
         print("\n Succesfully fetched historical data")
         print(historical_data.tail())
+
+        #Calculate 20 days SMA
+        print("\nCalculate SMA")
+        data_with_sma=moving_average.calculate_sma(historical_data, 20)
+
+        #Display SMA
+        print("\nData woth @)-day SMA")
+        print(data_with_sma.tail())
     else:
         print("\nFailed to fetch.")
     print("Data Fetch Completed")        
